ca5b172fdc05fcd45fe6223666e3542930ff0ab7
buzzcz <buzz3333@gmail.com>
2015-01-16 19:32:48 +0100

    Dodelano pro vice vstupnich stavu

--- src/main_pkg/Convert.java
+++ src/main_pkg/Convert.java
@@ -49,42 +49,49 @@ public class Convert {
 			}
 		}
 
-		// Prvni radka bude stejna jako v NKA
-		statuses.add("A");
-		for (int i = 0; i < nkaTable[0].length; i++) {
-			if (notUsed(sortString(nkaTable[0][i]))) {
-				statuses.add(sortString(nkaTable[0][i]));
-			}
+		// Prvni stav bude sjednoceni vstupnich stavu
+		String s = "";
+		for(int i = 0; i < nka.getInputStatusArray().length; i++) {
+			s += nka.getInputStatusArray()[i];
 		}
+		statuses.add(sortString(s));
 
-		//Pro kazdou radku se pro kazdy sloupec zjisti novy stav a pripadne prida do seznamu
+		// Pro kazdou radku se pro kazdy sloupec zjisti novy stav a pripadne
+		// prida do seznamu
 		int count = 0;
-		//Dokud nejsem na konci seznamu, tzn. uz nemam nove stavy
+		// Dokud nejsem na konci seznamu, tzn. uz nemam nove stavy
 		while (count < statuses.size()) {
-			String s = statuses.get(count);
-			//Pro kazdy sloupec v radku
+			s = statuses.get(count);
+			// Pro kazdy sloupec v radku
 			for (int i = 0; i < dkaTable[count].length; i++) {
-				//Pro kazdy puvodni stav z noveho stavu
+				// Pro kazdy puvodni stav z noveho stavu
 				for (int j = 0; j < s.length(); j++) {
 					String status = nkaTable[s.charAt(j) - 'A'][i];
-					//Kontroluji, jestli jiz stav neni v tomto sloupci
-					for(int k = 0; k < status.length(); k++) {
-						if(notUsed(dkaTable[count][i], status.charAt(k))) {
+					// Kontroluji, jestli jiz stav neni v tomto sloupci
+					for (int k = 0; k < status.length(); k++) {
+						if (notUsed(dkaTable[count][i], status.charAt(k))) {
 							dkaTable[count][i] += status.charAt(k);
 						}
 					}
 				}
-				//Nove vyplneny sloupec seradim podle abecedy
+				// Nove vyplneny sloupec seradim podle abecedy
 				dkaTable[count][i] = sortString(dkaTable[count][i]);
-				//Pokud stav ze sloupce jeste neni v seznamu, pridam ho
-				if(notUsed(dkaTable[count][i]) && !dkaTable[count][i].equals("")) {
+				// Pokud stav ze sloupce jeste neni v seznamu, pridam ho
+				if (notUsed(dkaTable[count][i])
+						&& !dkaTable[count][i].equals("")) {
 					statuses.add(dkaTable[count][i]);
 				}
 			}
 			count++;
 		}
-
-		//TODO upravit tabulku, aby nemela prazdne radky (2^pocetStavu) a vytvorit DKA s atributy...
+		for(int i = 0; i < dkaTable.length; i++) {
+			for(int j = 0; j < dkaTable[i].length; j++) {
+				System.out.println(dkaTable[i][j]);
+			}
+			System.out.println();
+		}
+		// TODO upravit tabulku, aby nemela prazdne radky (2^pocetStavu) a
+		// vytvorit DKA s atributy...
 		return dka;
 	}
 
@@ -106,8 +113,11 @@ public class Convert {
 
 	/**
 	 * Zjisti, zda string obsahuje znak
-	 * @param s String, ve kterem hledam
-	 * @param c Znak, ktery hledam
+	 * 
+	 * @param s
+	 *            String, ve kterem hledam
+	 * @param c
+	 *            Znak, ktery hledam
 	 * @return True pokud neobsahuje, false pokud ano
 	 */
 	private static boolean notUsed(String s, char c) {
