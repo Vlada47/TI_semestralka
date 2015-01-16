b4645c1ddebfe259f97c602c60c4459a8eaecd13
buzzcz <buzz3333@gmail.com>
2015-01-16 15:39:31 +0100

    Uprava cesty, protoze ja na Linuxu mam /, ne \

--- src/main_pkg/Main.java
+++ src/main_pkg/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
 	
-	public static final String FILE_PATH = "data\\automat.txt";
+	public static final String FILE_PATH = "data/automat.txt";
 
 	public static void main(String[] args) {
 		
@@ -12,6 +12,8 @@ public class Main {
 			Automaton a = Input_Output.createAutomatonFromFile(FILE_PATH);
 			
 			System.out.println(a.getAutomatonType());
+			
+			a = Convert.nkaToDka(a);
 		}
 		catch(IOException e) {
 			System.err.println("Chyba pri cteni souboru: "+e.getMessage());
@@ -19,8 +21,5 @@ public class Main {
 		catch(NumberFormatException f) {
 			System.err.println("Chyba pri prevadeni cisel ze Stringu na Integer: "+f.getMessage());
 		}
-		
-		
 	}
-
 }
